08e2e478e1335b8cd8acae060c2c28b68202629e
wanxuanc <wanxuanc@nycstudents.net>
2024-10-20 22:57:23 -0400

    finished

--- Counter App/script.js
+++ Counter App/script.js
@@ -5,4 +5,12 @@ function increment() {
     count++
     document.getElementById("count-el").innerHTML = count
 }
-   
+
+function save() {
+    let count = document.getElementById("count-el").innerHTML
+    let preCount = count + " - "
+    document.getElementById("preCount-el").innerHTML += preCount
+}
+function prank() {
+    document.getElementById("prank-btn").innerHTML = "Congrats! You get nothing!"
+}
--- calcJS/index.js
+++ calcJS/index.js
@@ -1,6 +1,7 @@
 let num1 = 8
 let num2 = 6
-
+document.getElementById("num1-el").innerHTML = num1
+document.getElementById("num2-el").innerHTML = num2
 function add(){
     document.getElementById("res-el").innerHTML = (num1 + num2)
 }
